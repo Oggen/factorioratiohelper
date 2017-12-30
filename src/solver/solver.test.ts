7d3b4ae5eb4931e97a8a3a042dcd01977b38fe47
Dan Miller <dan.miller@centare.com>
2017-12-30 08:43:36 -0600

    modified solver and added tests to support production chains up to mineral sludge

--- src/solver/solver.test.ts
+++ src/solver/solver.test.ts
@@ -6,44 +6,228 @@ import RecipeSolver  from './solver';
 import { SolutionModel }  from './solver';
 import { Step, ResourceCount, Resource } from '../datacomponents/index';
 
+const recipeDictionary: {[name: string]: Step} = {
+    'electrolysis': {
+        count: 0,
+        speed: 1,
+        time: 2,
+        inputs: [
+            {resource: 'water', count: 100},
+        ],
+        outputs: [
+            {resource: 'slag', count: 1},
+            {resource: 'oxygen', count: 30},
+            {resource: 'hydrogen', count: 40},
+        ]
+    },
+    'slag_crush': {
+        count: 0,
+        speed: 1.5,
+        time: 1,
+        inputs: [
+            {resource: 'slag', count: 1},
+        ],
+        outputs: [
+            {resource: 'crushed_stone', count: 2},
+        ]
+    },
+    'mineralize_water': {
+        count: 0,
+        speed: 1.5,
+        time: 1,
+        inputs: [
+            {resource: 'crushed_stone', count: 10},
+            {resource: 'water', count: 100},
+        ],
+        outputs: [
+            {resource: 'mineralized_water', count: 100},
+        ]
+    },
+    'green_algea_growth': {
+        count: 0,
+        speed: 1,
+        time: 20,
+        inputs: [
+            {resource: 'carbon_dioxide', count: 100},
+            {resource: 'mineralized_water', count: 100},
+        ],
+        outputs: [
+            {resource: 'green_algea', count: 40},
+        ]
+    },
+    'cellulose_fiber': {
+        count: 0,
+        speed: 1.5,
+        time: 3,
+        inputs: [
+            {resource: 'green_algea', count: 10},
+        ],
+        outputs: [
+            {resource: 'cellulose_fiber', count: 5},
+        ]
+    },
+    'wood_pellet': {
+        count: 0,
+        speed: 0.5,
+        time: 4,
+        inputs: [
+            {resource: 'cellulose_fiber', count: 12},
+        ],
+        outputs: [
+            {resource: 'wood_pellets', count: 2},
+        ]
+    },
+    'burn_pellets': {
+        count: 0,
+        speed: 1.5,
+        time: 2,
+        inputs: [
+            {resource: 'wood_pellets', count: 1},
+        ],
+        outputs: [
+            {resource: 'carbon_dioxide', count: 70},
+        ]
+    },
+    'slag_slurry': {
+        count: 0,
+        speed: 1.25,
+        time: 3,
+        inputs: [
+            {resource: 'slag', count: 5},
+            {resource: 'sulfuric_acid', count: 12},
+        ],
+        outputs: [
+            {resource: 'slag_slurry', count: 50},
+        ]
+    },
+    'sulfur_dioxide_to_acid': {
+        count: 0,
+        speed: 1.25,
+        time: 2,
+        inputs: [
+            {resource: 'sulfur_dioxide_gas', count: 60},
+            {resource: 'purified_water', count: 40},
+        ],
+        outputs: [
+            {resource: 'sulfuric_acid', count: 50},
+        ]
+    },
+    'sulfur_burning': {
+        count: 0,
+        speed: 1.25,
+        time: 2,
+        inputs: [
+            {resource: 'sulfur', count: 1},
+            {resource: 'oxygen', count: 50},
+        ],
+        outputs: [
+            {resource: 'sulfur_dioxide_gas', count: 50},
+        ]
+    },
+    'water_synthesis': {
+        count: 0,
+        speed: 1.25,
+        time: 2,
+        inputs: [
+            {resource: 'oxygen', count: 60},
+            {resource: 'hydrogen', count: 90},
+        ],
+        outputs: [
+            {resource: 'purified_water', count: 100},
+        ]
+    },
+    'sulfuric_waste_purification': {
+        count: 0,
+        speed: 1,
+        time: 1,
+        inputs: [
+            {resource: 'sulfuric_waste_water', count: 100},
+        ],
+        outputs: [
+            {resource: 'sulfur', count: 1},
+            {resource: 'mineralized_water', count: 20},
+            {resource: 'purified_water', count: 70},
+        ]
+    },
+    'slurry_coal_filtering': {
+        count: 0,
+        speed: 1.5,
+        time: 4,
+        inputs: [
+            {resource: 'coal_filter', count: 1},
+            {resource: 'slag_slurry', count: 50},
+            {resource: 'purified_water', count: 50},
+        ],
+        outputs: [
+            {resource: 'filter_frame', count: 1},
+            {resource: 'mineral_sludge', count: 50},
+            {resource: 'sulfuric_waste_water', count: 30},
+        ]
+    },
+    'coal_filter': {
+        count: 0,
+        speed: 0.5,
+        time: 1,
+        inputs: [
+            {resource: 'coal', count: 1},
+            {resource: 'filter_frame', count: 5},
+        ],
+        outputs: [
+            {resource: 'coal_filter', count: 5},
+        ]
+    },
+    'wood_brick_from_pellet': {
+        count: 0,
+        speed: 0.5,
+        time: 2,
+        inputs: [
+            {resource: 'wood_pellets', count: 8},
+        ],
+        outputs: [
+            {resource: 'wood_bricks', count: 4},
+        ]
+    },
+    'wood_brick_smelting': {
+        count: 0,
+        speed: 1,
+        time: 3.5,
+        inputs: [
+            {resource: 'wood_bricks', count: 1},
+        ],
+        outputs: [
+            {resource: 'coal', count: 3},
+        ]
+    },
+    'iron_smelting_from_ore': {
+        count: 0,
+        speed: 1,
+        time: 10.5,
+        inputs: [
+            {resource: 'iron_ore', count: 4},
+        ],
+        outputs: [
+            {resource: 'iron_plate', count: 3},
+        ]
+    },
+    'gear':{
+        count: 0,
+        speed: 0.5,
+        time: 0.5,
+        inputs: [
+            {resource: 'iron_plate', count: 2},
+        ],
+        outputs: [
+            {resource: 'gear', count: 1},
+        ]
+    }
+}
+
 describe('Recipe Solver', () => {
-    it('Solves a trivial linear non-branching crafting chain', () => {
+    it('Solves iron ore to gear', () => {
         // Arrange
         var stepsInput: Step[] = [
-            {
-                count: 0,
-                speed: 1,
-                time: 1,
-                inputs: [
-                    {
-                        resource: 'iron_ore',
-                        count: 1
-                    }
-                ],
-                outputs: [
-                    {
-                        resource: 'iron_plate',
-                        count: 1
-                    }
-                ]
-            },
-            {
-                count: 0,
-                speed: 1,
-                time: 1,
-                inputs: [
-                    {
-                        resource: 'iron_plate',
-                        count: 2
-                    }
-                ],
-                outputs: [
-                    {
-                        resource: 'gear',
-                        count: 1
-                    }
-                ]
-            },
+            recipeDictionary['iron_smelting_from_ore'],
+            recipeDictionary['gear'],
         ];
         var outputs: ResourceCount[] = [
             {resource: 'gear', count: 1}
@@ -54,10 +238,141 @@ describe('Recipe Solver', () => {
         var solver = new RecipeSolver(stepsInput, outputs, inputs);
         
         var expectedOutput: SolutionModel = {
-            stepMultipliers: [2, 1],
-            realInputs: [{resource: 'iron_ore', count: 2}],
+            stepMultipliers: [7, 1],
+            realInputs: [{resource: 'iron_ore', count: (8/3)}],
             extraOutputs: [],
         }
+
+        // Act
+        var result = solver.findSolution();
+
+        //Assert
+        //console.log(result);
+        expect(JSON.stringify(result) == JSON.stringify(expectedOutput)).toBeTruthy();
+    });
+    it('Solves water to mineralized water', () => {
+        // Arrange
+        var stepsInput: Step[] = [
+            recipeDictionary['electrolysis'],
+            recipeDictionary['slag_crush'],
+            recipeDictionary['mineralize_water'],
+        ];
+        var outputs: ResourceCount[] = [
+            {resource: 'mineralized_water', count: 1}
+        ];
+        var inputs: ResourceCount[] = [
+            {resource: 'water', count: 1}
+        ];
+        var solver = new RecipeSolver(stepsInput, outputs, inputs);
+        
+        var expectedOutput: SolutionModel = {
+            stepMultipliers: [0.1, 1/30, 2/300],
+            realInputs: [{resource: 'water', count: 6}],
+            extraOutputs: [
+                {resource: 'oxygen', count: 1.5},
+                {resource: 'hydrogen', count: 2},
+            ]
+        }
+
+        // Act
+        var result = solver.findSolution();
+
+        //Assert
+        //console.log(result);
+        expect(JSON.stringify(result) == JSON.stringify(expectedOutput)).toBeTruthy();
+    });
+    it('Solves water to wood pellets', () => {
+        // Arrange
+        var stepsInput: Step[] = [
+            recipeDictionary['electrolysis'],
+            recipeDictionary['slag_crush'],
+            recipeDictionary['mineralize_water'],
+            recipeDictionary['green_algea_growth'],
+            recipeDictionary['cellulose_fiber'],
+            recipeDictionary['wood_pellet'],
+            recipeDictionary['burn_pellets'],
+        ];
+        var outputs: ResourceCount[] = [
+            {resource: 'wood_pellets', count: 1}
+        ];
+        var inputs: ResourceCount[] = [
+            {resource: 'water', count: 1}
+        ];
+        var solver = new RecipeSolver(stepsInput, outputs, inputs);//, undefined, console.log);
+        
+        var expectedOutput: SolutionModel = {
+            stepMultipliers: [ 5.25, 1.75, 0.35, 10.5, 4.2, 7, 1 ],
+            realInputs: [{resource: 'water', count: 315}],
+            extraOutputs: [
+                {resource: 'oxygen', count: 78.75},
+                {resource: 'hydrogen', count: 105},
+            ]
+        }
+
+        // Act
+        var result = solver.findSolution();
+
+        //Assert
+        console.log(result);
+        expect(JSON.stringify(result) == JSON.stringify(expectedOutput)).toBeTruthy();
+    });
+    it('Solves water to mineral sludge', () => {
+        // Arrange
+        var stepsInput: Step[] = [
+            //wood pellet loop
+            recipeDictionary['electrolysis'],
+            recipeDictionary['slag_crush'],
+            recipeDictionary['mineralize_water'],
+            recipeDictionary['green_algea_growth'],
+            recipeDictionary['cellulose_fiber'],
+            recipeDictionary['wood_pellet'],
+            recipeDictionary['burn_pellets'],
+
+            recipeDictionary['wood_brick_from_pellet'],
+            recipeDictionary['wood_brick_smelting'],
+
+
+            recipeDictionary['slag_slurry'],
+            recipeDictionary['sulfur_dioxide_to_acid'],
+            recipeDictionary['sulfur_burning'],
+            recipeDictionary['water_synthesis'],
+            recipeDictionary['sulfuric_waste_purification'],
+            recipeDictionary['slurry_coal_filtering'],
+            recipeDictionary['coal_filter'],
+        ];
+        var outputs: ResourceCount[] = [
+            {resource: 'mineral_sludge', count: 100}
+        ];
+        var inputs: ResourceCount[] = [
+            {resource: 'water', count: 1}
+        ];
+        var solver = new RecipeSolver(stepsInput, outputs, inputs);//, undefined, console.log);
+        
+        var expectedOutput: SolutionModel = {
+            stepMultipliers:
+             [ 20.2,//electrolysis
+               0.06666666666666661,//slag crushers
+               0.013333333333333324,//water mineralizers
+               2.7999999999999994,//green algea ponds
+               1.1199999999999999,//cellulose fiber
+               1.8666666666666665,//wood pellet assemblers
+               0.2666666666666666,//pellet - to - co2
+               0.13333333333333333,// pellet to brick
+               0.4666666666666668,// wood brick to coal
+               4.8,// slag slurry assemblers
+               0.8,// sulfur dioxide to acid
+               0.96,// sulfur to sulfur dioxide
+               7.1822222222222205,// synthesis of purified water
+               0.6000000000000001, // sulfuric waste purifiers
+               5.333333333333334, // slag slurry filters
+               0.8 // coal filter assemblers
+            ],
+            realInputs: [ { resource: 'water', count: 1012 } ],
+            extraOutputs:
+             [ { resource: 'oxygen', count: 3.6666666666666874 },
+               { resource: 'sulfuric_acid', count: 1.0000000000000038 },
+               { resource: 'purified_water', count: 370.8888888888888 } ]
+            }
 
         // Act
         var result = solver.findSolution();
--- src/solver/solver.ts
+++ src/solver/solver.ts
@@ -2,13 +2,13 @@ import * as glpk from './glpk.min.js';
 
 import { Step, ResourceCount, Resource } from '../datacomponents/index';
 
-class RecipeModel{
+export class RecipeModel{
     resources: ResourceCount[];
     constructor(source: Step){
         this.resources = [];
 
         //the multiple of this recipe that is complete in 1 time unit
-        var productionRatio = source.time / source.speed;
+        var productionRatio = source.speed / source.time;
         source.inputs.forEach(value => {
             this.resources.push({count: -value.count * productionRatio, resource: value.resource})
         });
@@ -31,28 +31,51 @@ export class SolutionModel{
 }
 
 export default class RecipeSolver{
-
     private recipeData: RecipeModel[] = [];
     //private allResources: Resource;
     private desiredOutputs: ResourceCount[];
     private inputWeights: ResourceCount[];
+    private slackOutputs: ResourceCount[] | undefined;
+
+    private log: (str: string) => void;
 
     /**
      * 
      * @param allSteps All of the steps involved in this problem. can include steps not necissarily involved in the problem
      * @param desiredOutputs The outputs desired, in both name and amount
      * @param inputRatios All of the inputs that will be accepted; with an accompanying number that indcates the relative cost of the input
+     * @param slackOutputs The possible extra output resources, and the prefferential rate that they should be output into.
+     *      Similar to inputRations, except for outputs. If left out, then it is assumed all non-input and non-output resources are slack outputs
+     *      with a weight of 1,000,000,000
      */
-    constructor(allSteps: Step[], desiredOutputs: ResourceCount[], inputRatios: ResourceCount[]){
+    constructor(allSteps: Step[], desiredOutputs: ResourceCount[], inputRatios: ResourceCount[], slackOutputs?: ResourceCount[], log: (str: string) => void = a=>{}){
         //this.recipeData = allSteps;
         this.desiredOutputs = desiredOutputs;
         this.inputWeights = inputRatios;
+        this.slackOutputs = slackOutputs;
+        this.log = log;
 
         allSteps.forEach((step, i) => {
             this.recipeData.push(new RecipeModel(step));
         });
 
-        console.log(JSON.stringify(this.recipeData));
+        this.log(JSON.stringify(this.recipeData));
+    }
+
+    private allResources: Resource[];
+
+    private addResourcePipeWithWeight(resourceName: string, resourceWeight: number, isInput: boolean, glpkProblem: any){
+        var index = this.allResources.indexOf(resourceName) + 1;
+        if(index == 0)
+            throw "Error: prioritized resource pipe not found in any recipes";
+        
+        var colIndex = glpk.glp_add_cols(glpkProblem, 1);
+        //setup a new "recipe" that does nothing but produce the input item
+        glpk.glp_set_mat_col(glpkProblem, colIndex, 1, [0, index], [0, (isInput ? 1 : -1)]);
+        //Make sure that the column only has a lower bound
+        glpk.glp_set_col_bnds(glpkProblem, colIndex, glpk.GLP_LO, 0, 0);
+        //put a cost on the new recipe column, indicating the relative cost of using one input item
+        glpk.glp_set_obj_coef(glpkProblem, colIndex, resourceWeight);
     }
 
     public findSolution(): SolutionModel{
@@ -63,7 +86,7 @@ export default class RecipeSolver{
         //every recipe is a column in the solver matrix
         //glpk.glp_add_cols(problem, this.recipeData.length);
 
-        var allResources: Resource[] = [];
+        this.allResources = [];
         var colMetadata: ColumnMetadata[] = [];
 
         this.recipeData.forEach((recipe, i) => {
@@ -73,11 +96,11 @@ export default class RecipeSolver{
             var colValues: number[] = [];
 
             recipe.resources.forEach((resource: ResourceCount) => {
-                var index = allResources.indexOf(resource.resource) + 1;
+                var index = this.allResources.indexOf(resource.resource) + 1;
                 if(index == 0){
                     //if the resource hasn't been added, add it as a row
                     index = glpk.glp_add_rows(problem, 1);
-                    allResources.push(resource.resource);
+                    this.allResources.push(resource.resource);
                     glpk.glp_set_row_name(problem, index, resource.resource);
                     glpk.glp_set_row_bnds(problem, index, glpk.GLP_FX, 0, 0);
                 }
@@ -88,36 +111,49 @@ export default class RecipeSolver{
             glpk.glp_set_col_bnds(problem, colIndex, glpk.GLP_LO, 0, 0);
             glpk.glp_set_mat_col(problem, colIndex, rowIndexes.length, [0, ...rowIndexes], [0, ...colValues]);
 
-
         });
 
         this.inputWeights.forEach(resource => {
-            var index = allResources.indexOf(resource.resource) + 1;
-            if(index == 0)
-                throw "Error: prioritized input resource not found in any recipes";
-            
-            var colIndex = glpk.glp_add_cols(problem, 1);
-            //setup a new "recipe" that does nothing but produce the input item
-            glpk.glp_set_mat_col(problem, colIndex, 1, [0, index], [0, 1]);
-            //Make sure that the column only has a lower bound
-            glpk.glp_set_col_bnds(problem, colIndex, glpk.GLP_LO, 0, 0);
-            //put a cost on the new recipe column, indicating the relative cost of using one input item
-            glpk.glp_set_obj_coef(problem, colIndex, resource.count);
+            this.addResourcePipeWithWeight(resource.resource, resource.count, true, problem);
 
             colMetadata.push({input: resource})
         });
 
         this.desiredOutputs.forEach(resource => {
-            var index = allResources.indexOf(resource.resource) + 1;
+            var index = this.allResources.indexOf(resource.resource) + 1;
             if(index == 0)
                 throw "Error: output resource not found in any recipes";
             glpk.glp_set_row_bnds(problem, index, glpk.GLP_FX, resource.count, resource.count);
         });
 
+        if(this.slackOutputs){
+            this.slackOutputs.forEach(resource => {
+                this.addResourcePipeWithWeight(resource.resource, resource.count, false, problem);
+
+                colMetadata.push({output: resource})
+            });
+        }else{
+            // if slackOutputs are null; assume all resources are slack outputs at very high weight
+            this.allResources.forEach((resource, index) => {
+                if(this.inputWeights.reduce((pv, current) => pv || (current.resource == resource), false)){
+                    // if the resource is already an input
+                    return;
+                }
+                if(this.desiredOutputs.reduce((pv, current) => pv || (current.resource == resource), false)){
+                    // if the resource is already an output
+                    return;
+                }
+                
+                this.addResourcePipeWithWeight(resource, 1000000000, false, problem);
+
+                colMetadata.push({output: {resource: resource, count: 1000000000}});
+            })
+        }
+
         var file = "";
-        var callbk = (str: string) => {file = file + str + "\n"}
+        var callbk = (str: string) => {file += str + "\n"}
         glpk.glp_write_lp(problem, null, callbk);
-        console.log(file);
+        this.log(file);
 
         var result = glpk.glp_simplex(problem);
         if(result != 0){
@@ -129,13 +165,15 @@ export default class RecipeSolver{
         
         colMetadata.forEach((meta, i) => {
             var value = glpk.glp_get_col_prim(problem, i + 1);
-            console.log(value);
-            if(meta.input){
-                output.realInputs.push({resource: meta.input.resource, count: value});
-            }else if(meta.output){
-                output.extraOutputs.push({resource: meta.output.resource, count: value});
-            }else if(meta.recipe){
-                output.stepMultipliers.push(value);
+            this.log(value);
+            if(value > 0){
+                if(meta.input){
+                    output.realInputs.push({resource: meta.input.resource, count: value});
+                }else if(meta.output){
+                    output.extraOutputs.push({resource: meta.output.resource, count: value});
+                }else if(meta.recipe){
+                    output.stepMultipliers.push(value);
+                }
             }
         })
 
